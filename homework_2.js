da644a3eed04453d492e2ff111d0ec1d5d1d8248
palmerok1489 <skinnyhardstyle@mail.ru>
2023-04-16 21:47:13 +0300

    вроде бы сделал,но не знаю как 24 строчку сделать

---
+++ homework_2.js
@@ -0,0 +1,25 @@
+let money = prompt("Ваш месячный доход?");
+let expenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую?");
+let amount = prompt("Во сколько обойдуться обязательые статьи расходов?");
+let deposit = confirm("Есть ли у вас вклад в банке?");
+if (result){
+    document.querySelector("#result").textContent = "Да";
+} else {
+    document.querySelector("#result").textContent = "Нет";
+}
+
+let budgetMonth = 12875.3;
+console.log(budgetMonth - 3129.8);
+let purpose = 12824.4 ;
+console.log(Math.ceil (purpose));
+let budgetDay = 19.34;
+console.log(Math.floor(budgetDay));
+if (budgetDay > 100000){
+    console.log("У вас высокиий уровень дохода");
+} else if (budgetDay >= 30000){
+    console.log("У вас средний уровень дохода");
+} else if (budgetDay <= 30000 ) {
+    console.log("К сожалению у вас уровень дохода ниже среднего");
+} else {
+    console.log("что то пошло не так");
+}
