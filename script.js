4ee9bdb23f38ea6dd6abf720b1774d2fc9386cbf
palmerok1489 <skinnyhardstyle@mail.ru>
2023-04-25 22:51:48 +0300

    простой калькулятор

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let op;
+
+function func() {
+    let result;
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+    document.getElementById("result").innerHTML = result;
+}
+
+
