2fbf888121e158f2f66320e042ab8e80bef97cc7
palmerok1489 <skinnyhardstyle@mail.ru>
2023-07-04 22:59:38 +0300

    добавил циклы в дз

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
 let personalMovieDB = {
   count: ['numberOfFilms'],
   movies: {},
@@ -12,11 +11,27 @@ let personalMovieDB = {
   privat: false
 };
 
-let a = prompt("Один из последних просмотренных фильмов", '');
-let b = prompt ('На сколько оцените его?','');
-let c = prompt("Один из последних просмотренных фильмов", '');
-let d = prompt ('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
+for(let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов", '');
+  let b = prompt ('На сколько оцените его?','');
+  
+  if (a != null && b != null && a != '' && b != "" && a.length < 50){
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+  personalMovieDB.movies[a] = b;
+}
 
+if (personalMovieDB.count < 10) {
+  console.log ('Просмотренно довольно много фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log ('Вы класический зритель');
+} else if (personalMovieDB.count >= 30 ) {
+  console.log ('Да вы киоман!');
+} else {
+  console.log ('Произошла ошибка');
+}
 
+console.log (personalMovieDB);
